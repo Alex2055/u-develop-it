903930b57692fff9fa7ec00d7e0e3d75558235d7
Alex2055 <alexfromnashville@gmail.com>
2021-04-15 14:06:48 -0500

    SQL APIs

--- server.js
+++ server.js
@@ -1,30 +1,25 @@
 const express = require('express');
+const db = require('./db/database');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
-const sqlite3 = require('sqlite3').verbose();
+
+const apiRoutes = require('./routes/apiRoutes');
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Connect to database
-const db = new sqlite3.Database('./db/election.db', err => {
-    if (err) {
-      return console.error(err.message);
-    }
-  
-    console.log('Connected to the election database.');
-  });
+// Use apiRoutes
+app.use('/api', apiRoutes);
 
-
-
-// Default response for any other request(Not Found) Catch all
+// Default response for any other request(Not Found) Catch all other
 app.use((req, res) => {
-    res.status(404).end();
-  });
+  res.status(404).end();
+});
 
-
-  db.on('open', () => {
-app.listen(PORT, () => {
+// Start server after DB connection
+db.on('open', () => {
+  app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
 });
